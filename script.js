e2357337018a3a4e953971da40c8f03e33cb7b34
Nick Romano <njr7romano@yahoo.com>
2020-03-18 11:54:01 -0400

    added .each() function to loop through and colorize time blocks, also adds textareas for future events

--- script.js
+++ script.js
@@ -6,5 +6,44 @@ $( document ).ready(function() {
 
     var time = moment().format('LT'); 
 
+    console.log(time);
+
+    var colon = time.indexOf(":");
+
+    var hour = time.substring(0, colon);
+
+    console.log("hour: ", hour);
+
+    var M = time.indexOf("M") + 1;
+
+    var A = M - 2;
+
+    var amOrPm = time.substring(A, M);
+
+    var oClock = hour + amOrPm;
+
+    console.log("hour: " + oClock);
+
+    var currentHourSelected = false;
+
+    $(".table.table-hover tbody tr").each(function(index) {
+        var trVal = $(this).attr("value");
+        console.log(index);
+
+        if (trVal === oClock) {
+            console.log("The top of the current hour is: " + trVal);
+            $(this).children(".event").addClass("table-warning");
+            currentHourSelected = true;
+        }
+        else if (currentHourSelected === false) {  
+            $(this).children(".event").addClass("table-secondary");
+        }
+        else {
+            $(this).children(".event").addClass("table-success")
+            .append('<textarea id="' + oClock + '" placeholder="Enter an event name and details"></textarea>');
+        }
+
+    });
+
 });
 
